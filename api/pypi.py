754db2d932ad73b5b46bdc6b92c9d25a9c5d7b05
Monique da Silva <monique_ds@icloud.com>
2021-11-06 01:54:01 +0000

    teste

--- api/pypi.py
+++ api/pypi.py
@@ -10,24 +10,23 @@ import requests
 def version_exists(package_name, version):
     # TODO
     # Fazer requisição na API do PyPI para checar se a versão existe
-    
     base_url = "https://pypi.org/pypi"
     url = f'{base_url}/{package_name}/{version}/json'
-    
+
     response = requests.get(url)
     if response.status_code == 404:
         return False
-    
-  return True
-    
+
+    return True
+
+
 def latest_version(package_name):
     # TODO
     # Fazer requisição na API do PyPI para descobrir a última versão
     # de um pacote. Retornar None se o pacote não existir.
-    
     base_url = "https://pypi.org/pypi"
     url = f'{base_url}/{package_name}/json'
-    
+
     response = requests.get(url)
     if response.status_code == 404:
         return None
--- api/serializers.py
+++ api/serializers.py
@@ -1,54 +1,38 @@
 from rest_framework import serializers
-
 from .models import PackageRelease, Project
 from .pypi import version_exists, latest_version
-
-
 class PackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PackageRelease
         fields = ["name", "version"]
         extra_kwargs = {"version": {"required": False}}
-
     def validate(self, data):
-        # TODO
-        # Validar o pacote, checar se ele existe na versão especificada.
-        # Buscar a última versão caso ela não seja especificada pelo usuário.
-        # Subir a exceção `serializers.ValidationError()` se o pacote não
-        # for válido.
-        
+      
         name = data.get('name', '')
         version = data.get('version', '')
-        
         if version:
-            exist = version_exists(name, version)        
-            
+            exist = version_exists(name, version)
             if exist:
                 return data
             else:
-                raise serializers.ValidationError({"error: "One or more packages doesn't exist"})
-         else:
-             latest = latest_version(name)
-             
-             if latest:
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
+        else:
+            latest = latest_version(name)
+            if latest:
                 data['version'] = latest
                 return data
-             else:
-                raise serializers.ValidationError({"error: "One or more packages doesn't exist"})
-
+            else:
+                raise serializers.ValidationError({"error": "One or more packages doesn't exist"})
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ["name", "packages"]
-
     packages = PackageSerializer(many=True)
-
     def create(self, validated_data):
-        # TODO
-        # Salvar o projeto e seus pacotes associados.
-        #
-        # Algumas referência para uso de models do Django:
-        # - https://docs.djangoproject.com/en/3.2/topics/db/models/
-        # - https://www.django-rest-framework.org/api-guide/serializers/#saving-instances
+       
         packages = validated_data["packages"]
-        return Project(name=validated_data["name"])
+
+        projeto = Project.objects.create(name=validated_data["name"])  
+        for package in packages:
+            PackageRelease.objects.create(project=projeto, name=package['name'], version=package['version'])
+        return projeto
