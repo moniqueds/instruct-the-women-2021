f4d30e9823ce6d8ce0d83e48e72f3025f0324e44
Monique da Silva <89393449+moniqueds@users.noreply.github.com>
2021-11-05 22:42:11 -0300

    Update pypi.py

--- api/pypi.py
+++ api/pypi.py
@@ -10,11 +10,28 @@ import requests
 def version_exists(package_name, version):
     # TODO
     # Fazer requisição na API do PyPI para checar se a versão existe
-    return False
+    base_url = "https://pypi.org/pypi"
+    url = f'{base_url}/{package_name}/{version}/json'
+
+    response = requests.get(url)
+    if response.status_code == 404:
+        return False
+
+    return True
 
 
 def latest_version(package_name):
     # TODO
     # Fazer requisição na API do PyPI para descobrir a última versão
     # de um pacote. Retornar None se o pacote não existir.
-    return "?"
+    base_url = "https://pypi.org/pypi"
+    url = f'{base_url}/{package_name}/json'
+
+    response = requests.get(url)
+    if response.status_code == 404:
+        return None
+    
+    response = response.json()
+    version = response['info']['version']
+    
+    return version
